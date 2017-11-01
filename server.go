566c0922b1c8242ebce0e92976a24e811817e36d
zaru <sakuraba@basicinc.jp>
2017-11-01 14:57:24 +0900

    🐼:

--- server.go
+++ server.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	e := echo.New()
 	e.Static("/vue", "vue-app/dist")
+	e.Static("/vue-org", "vue-app")
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!2")
 	})
