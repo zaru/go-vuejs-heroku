44690c15096a673dce1afde244f6ac8d5f029a8d
zaru <sakuraba@basicinc.jp>
2017-11-01 14:33:22 +0900

    Fixed os.Getenv(PORT)

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
 )
@@ -11,5 +12,5 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
 }
