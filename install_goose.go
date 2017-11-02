2b86e8eeb49196fca7fe5de376600ba0943e555b
zaru <sakuraba@basicinc.jp>
2017-11-02 12:29:35 +0900

    🐒

--- install_goose.go
+++ install_goose.go
@@ -1,8 +1,8 @@
 // +build heroku
 
-package main
+package goose2
 
 import (
 	// Invoke init() functions within migrations pkg.
-	_ "github.com/zaru/go-vuejs-heroku/cmd"
+	_ "github.com/zaru/go-vuejs-heroku/cmd/goose"
 )
